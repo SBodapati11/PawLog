c5f0f54dc2d3382e9f9fb03a8317dfe77f4de0a5
Viswajith Rajagopalan <57506139+ViswajithRajagopalan@users.noreply.github.com>
2021-01-14 21:36:31 -0600

    Update main_handler.py

--- main_handler.py
+++ main_handler.py
@@ -3,13 +3,16 @@ newFile = open('main.pl', 'w+')
 
 newFile.write("#include 'PawLog.pl'.\n\n")
 
-size = input ("What size dog would you prefer? (S/M/L/NA): ").lower()
-while(size not in ['s', 'm', 'l', 'na']):
-    size = input ("Please input a valid option.\nWhat size dog would you prefer? (S/M/L/NA): ").lower()
+size = input ("What size dog would you prefer? (Small, Medium, Large): ").lower()
+while(size not in ['small', 'medium', 'large']):
+    size = input ("Please input a valid option.\nWhat size dog would you prefer? (Small, Medium, Large): ").lower()
 
-dwelling = input ("What kind of housing do you have? (Apartment, Small House, Large House): ").lower()
-while(dwelling not in ['apartment', 'small house', 'large house']):
-    dwelling = input ("Please input a valid option.\nWhat kind of housing do you have? (Apartment, Small House, Large House): ").lower()
+place = input ("What kind of housing do you have? (Apartment, Small House, Large House): ").lower()
+while(place not in ['apartment', 'small house', 'large house']):
+    place = input ("Please input a valid option.\nWhat kind of housing do you have? (Apartment, Small House, Large House): ").lower()
+
+if(place != 'apartment'):
+    place = place[:5] 
 
 kids = input ("Do you have kids? (Yes/No): ").lower()
 while(kids not in ['yes', 'no']):
@@ -19,19 +22,19 @@ allergies = input ("Do you have allergies? (Yes/No): ").lower()
 while(allergies not in ['yes', 'no']):
     allergies = input ("Please input a valid option.\nDo you have allergies? (Yes/No): ").lower()
 
-dog = input ("Have you owned a dog before? (Yes/No): ").lower()
-while(dog not in ['yes', 'no']):
-    dog = input ("Please input a valid option.\nHave you owned a dog before? (Yes/No): ").lower()
-
 pets = input ("Do you have any other pets? (Yes/No): ").lower()
 while(pets not in ['yes', 'no']):
     pets = input ("Please input a valid option.\nDo you have any other pets? (Yes/No): ").lower()
+
+dog = input ("Have you owned a dog before? (Yes/No): ").lower()
+while(dog not in ['yes', 'no']):
+    dog = input ("Please input a valid option.\nHave you owned a dog before? (Yes/No): ").lower()
 
 outdoors = input ("How do you feel about the outdoors? (1-5): ").lower()
 while(outdoors not in ['1', '2', '3', '4', '5']):
     outdoors = input ("Please input a valid option.\nHow do you feel about the outdoors? (1-5): ").lower()
 
-fileString = "?- _main([" + size + ", " + dwelling  + ", " + kids + ", " + allergies + ", " + dog + ", " + pets + ", " + outdoors + "])." + "\n"
+fileString = "?- _main([" + size + ", " + place  + ", " + kids + ", " + allergies + ", " + dog + ", " + pets + ", " + outdoors + "])." + "\n"
 
 newFile.write(fileString)
 
